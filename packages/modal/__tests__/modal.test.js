0a8184ca54109f31dfec7d832a912166a394a95e
george <hello@geotrev.com>
2020-08-29 17:20:13 -0700

    add package files

---
+++ packages/modal/__tests__/modal.test.js
@@ -0,0 +1,5 @@
+import "../src"
+
+describe("@spider-ui/modal", () => {
+  it("needs tests", () => {})
+})
---
+++ packages/modal/src/constants.js
@@ -0,0 +1,12 @@
+export const Attributes = {}
+
+export const ClassNames = {
+  VISIBLE: "visible",
+}
+
+export const Slots = {
+  TRIGGER: "[slot='trigger']",
+  CONTENT: "[slot='content']",
+}
+
+export const ESCAPE_KEY = "Escape"
